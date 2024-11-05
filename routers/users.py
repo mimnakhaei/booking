aa0f1343befb34eb876c8ce43ffeff1e0cc222b1
0ne-zero <pouria.khakpour9909@gmail.com>
2024-11-05 20:20:57 +0330

    /bookings endpoint

--- routers/users.py
+++ routers/users.py
@@ -1,12 +1,13 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from database.hash import Hash
 
-from database.model.user.api import User, create_user, update_user
-
-from schema import UserBase, UserDisplay
+from database.model.user.api import User
+import database.model.user.api as user_api
+import database.model.booking.api as booking_api
+from schema import UserBase,  BookingDisplay
 from database.init.db import get_db
 from auth import authenticate, Role
+from schema.booking import create_booking_display
 
 router = APIRouter(prefix="/user", tags=["Users"])
 
@@ -20,15 +21,18 @@ def get_me(user: User = Depends(authenticate(Role.normal))):
 
 @router.post("/me", response_model=UserBase)
 def update_me(request: UserBase, user: User = Depends(authenticate(Role.normal)), db: Session = Depends(get_db)):
-    user = update_user(db, user.id, request)  # type: ignore
+    user = user_api.update_user(db, user.id, request)  # type: ignore
 
     return UserBase(username=user.username, email=user.email, password=user.password)  # type: ignore
 
 
-@router.get("/bookings", response_model=None)  # TODO: choose response model
-def get_bookings(user: User = Depends(authenticate(Role.normal))):
-    # TODO: implement
-    return None
+@router.get("/bookings", response_model=list[BookingDisplay])  # TODO: choose response model
+def get_bookings(user: User = Depends(authenticate(Role.normal)), db: Session = Depends(get_db)):
+    res: list[BookingDisplay] = []
+    bookings: list[booking_api.Booking] = booking_api.get_bookings_by_user_id(db, user.id)  # type: ignore
+    for booking in bookings:
+        res.append(create_booking_display(booking))
+    return res
 
 
 @router.delete("/bookings/{booking_id}", response_model=None)  # TODO: choose response model
