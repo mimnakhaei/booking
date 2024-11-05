78c65b155f9f6947af18bde651c04b7f7d95744c
0ne-zero <pouria.khakpour9909@gmail.com>
2024-11-05 21:14:46 +0330

    get all hotels

--- routers/hotels.py
+++ routers/hotels.py
@@ -4,11 +4,21 @@ from typing import List
 from database.init.db import get_db
 from schema import HotelCreate, HotelDisplay
 import database.model.hotel.api as hotel_api
+from schema.hotel import create_hotel_display
 
 router = APIRouter(
     prefix="/hotels",
     tags=["Hotels"]
 )
+
+
+@router.get("/", response_model=List[HotelDisplay])
+def get_all_hotels(db: Session = Depends(get_db)):
+    res: List[HotelDisplay] = []
+    for hotel in hotel_api.get_all_hotels(db):
+        item = create_hotel_display(hotel)
+        res.append(item)
+    return res
 
 
 @router.post("/", response_model=HotelDisplay)
@@ -22,11 +32,6 @@ def get_hotel(hotel_id: int, db: Session = Depends(get_db)):
     if not hotel:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Hotel not found")
     return hotel
-
-
-@router.get("/", response_model=List[HotelDisplay])
-def get_all_hotels(db: Session = Depends(get_db)):
-    return hotel_api.get_all_hotels(db)
 
 
 @router.put("/{hotel_id}", response_model=HotelDisplay)
